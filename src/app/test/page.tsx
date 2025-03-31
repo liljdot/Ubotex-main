e0f4e9dc00cd907a18c38ed08d2b41cce8d20ac8
liljdot <jehuoki@gmail.com>
2025-03-31 11:18:35 +0100

    Add environment display and update heading style on test page

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,10 @@
 const Test: React.FC = () => {
+const env = process.env.NODE_ENV
+    
     return (
         <>
-            <h1>This is the test Page</h1>
+            <h1 className="bg-amber-50">This is the test Page</h1>
+            <h1>environment: {env}</h1>
         </>
     )
 }
