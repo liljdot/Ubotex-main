ec071f9a9caddbae4f85035186c9f62cd89155cb
liljdot <jehuoki@gmail.com>
2025-04-06 18:49:41 +0100

    Enhance ActionButton and HeroSection styles for improved responsiveness and layout

--- src/app/components/ActionButton.tsx
+++ src/app/components/ActionButton.tsx
@@ -17,7 +17,7 @@ interface Props {
 const ActionButton: React.FC<Props> = ({ className, children, to }) => {
 
     return (
-        <Link href={to} className={openSans.className + " btn min-w-26.5 text-[0.6rem] h-8 py-2 border-none shadow-none rounded-full hover:scale-105 transition-all ease-in-out duration-300 " + className}>
+        <Link href={to} className={openSans.className + " btn min-w-26.5 md:min-w-36 lg:min-w-44 text-[0.6rem] md:text-sm lg:text-base font-normal h-8 md:h-12 lg:h-14 py-2 md:py-3 lg:py-4 border-none shadow-none rounded-full hover:scale-105 transition-all ease-in-out duration-300 " + className}>
             {children}
         </Link>
     )
@@ -26,7 +26,7 @@ const ActionButton: React.FC<Props> = ({ className, children, to }) => {
 export const DomainActionButton: React.FC<Props> = ({className, children, to}) => {
 
     return (
-        <DomainLink subDomain={to} className={openSans.className + " btn min-w-26.5 text-[0.6rem] h-8 py-2 border-none shadow-none rounded-full hover:scale-105 transition-all ease-in-out duration-300 " + className}>
+        <DomainLink subDomain={to} className={openSans.className + " btn min-w-26.5 md:min-w-36 lg:min-w-44 text-[0.6rem] md:text-sm lg:text-base font-normal h-8 md:h-12 lg:h-14 py-2 md:py-3 lg:py-4 border-none shadow-none rounded-full hover:scale-105 transition-all ease-in-out duration-300 " + className}>
             {children}
         </DomainLink>
     )
--- src/app/components/home/HeroSection.tsx
+++ src/app/components/home/HeroSection.tsx
@@ -14,7 +14,7 @@ const openSans = Open_Sans({
 const CardImage: React.FC<{image: StaticImageData}> = ({image}) => {
 
     return (
-        <figure className="flex shrink-0 w-51 h-39 md:w-93 md:h-100 bg-cover md:bg-center md:hover:bg-left-top bg-no-repeat md:hover:w-143 transition-all ease-in-out duration-300" style={{backgroundImage: `url(${image.src})`}}>
+        <figure className="flex shrink-0 w-51 h-39 sm:w-60 sm:h-50 md:w-60 md:h-50 lg:w-84 lg:h-72 xl:w-93 xl:h-100 bg-cover md:bg-center md:hover:bg-left-top bg-no-repeat md:hover:w-64 lg:hover:w-100 xl:hover:w-143 transition-all ease-in-out duration-300" style={{backgroundImage: `url(${image.src})`}}>
             
         </figure>
     )
@@ -24,24 +24,24 @@ const HeroSection: React.FC = () => {
 
     return (
         <>
-            <section className="bg-neutral flex flex-col gap-9">
-                <div className="flex flex-col items-center gap-5 bg-contain bg-no-repeat px-12 pt-19" style={{ backgroundImage: `url(${blueprintBG.src})` }}>
-                    <h1 className="text-primary text-3xl text-center">
+            <section className="bg-neutral flex flex-col gap-9 md:gap-0">
+                <div className="flex flex-col items-center gap-5 md:gap-8 lg:gap-10 bg-contain bg-no-repeat px-12 md:px-24 lg:px-32 xl:px-75.5 pt-19 md:pt-28 lg:pt-37.5" style={{ backgroundImage: `url(${blueprintBG.src})` }}>
+                    <h1 className="text-primary text-3xl md:text-4xl lg:text-7xl md:font-semibold text-center">
                         More Than Business,
                         <br />
                         A Legacy of Excellence
                     </h1>
 
-                    <p className={`text-primary text-[0.75rem] text-center ${openSans.className}`}>
+                    <p className={`text-primary text-[0.75rem] md:text-base lg:max-w-153 text-center ${openSans.className}`}>
                         From quality building materials to exceptional hospitality and fine wines, Ubotex Limited turns aspirations into reality. Whether you're constructing, unwinding, or celebrating, we deliver excellence at every step.
                     </p>
 
                     <div className="flex flex-row justify-center">
-                        <ActionButton to="" className="bg-primary text-neutral text-[0.625rem]">Explore</ActionButton>
+                        <ActionButton to="" className="bg-primary text-neutral">Explore</ActionButton>
                     </div>
                 </div>
 
-                <div className="flex md:justify-center flex-row gap-2 overflow-x-scroll md:overflow-visible">
+                <div className="md:bg-gradient-to-b from-neutral via-neutral via-50% to-primary to-50% flex md:justify-center flex-row gap-2 md:gap-4 md:py-5 lg:py-10 xl:py-20 overflow-x-scroll md:overflow-visible">
                     <CardImage image={constructionImage}/>
                     <CardImage image={hotelImage}/>
                     <CardImage image={wineStoreImage}/>
