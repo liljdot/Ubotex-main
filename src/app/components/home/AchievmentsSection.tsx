04f0bc91ad0f400c8ef3819d16f73cde5bd5795b
liljdot <jehuoki@gmail.com>
2025-04-06 19:49:50 +0100

    Enhance AchievementsSection layout and styling for improved responsiveness and readability

--- src/app/components/home/AchievmentsSection.tsx
+++ src/app/components/home/AchievmentsSection.tsx
@@ -21,14 +21,14 @@ interface AchievementCardProps {
 const AchievementCard: React.FC<AchievementCardProps> = ({ Icon, details }) => {
 
     return (
-        <div className="w-full bg-[#EBF6FF] flex flex-row gap-0.5 items-center justify-start pl-4 py-7.5">
-            <div className="bg-neutral size-6 flex justify-center items-center rounded-full p-1">
+        <div className="w-full bg-[#EBF6FF] md:bg-neutral flex flex-row gap-0.5 items-center justify-start md:justify-center pl-4 md:pl-0 py-7.5 md:py-0">
+            <div className="bg-neutral size-6 md:size-10 lg:size-13 flex justify-center items-center rounded-full p-1 md:p-2.5">
                 <Icon className="text-primary size-full" />
             </div>
 
             <div className={"flex flex-col text-primary " + openSans.className}>
-                <h6 className="font-semibold">{details.value}</h6>
-                <p className="text-sm">{details.achievement}</p>
+                <h6 className="md: text-xl lg:text-3xl font-semibold">{details.value}</h6>
+                <p className="text-sm md:text-base">{details.achievement}</p>
             </div>
         </div>
     )
@@ -38,7 +38,7 @@ const AchievementsSection: React.FC = () => {
 
     return (
         <>
-            <section className="bg-neutral grid grid-cols-2 gap-[1px] my-15">
+            <section className="bg-neutral grid grid-cols-2 md:grid-cols-4 gap-[1px] my-15 md:my-13">
                 <AchievementCard Icon={FaHelmetSafety} details={{value: "500+", achievement: "Workers Employed"}}/>
                 <AchievementCard Icon={MdPeopleAlt} details={{value: "2k+", achievement: "Satisfied Customers"}}/>
                 <AchievementCard Icon={RiServiceFill} details={{value: "5k+", achievement: "Services Provided"}}/>
