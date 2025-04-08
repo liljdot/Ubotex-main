8e936a82fbf48baa43168737022ca841c1a0a315
liljdot <jehuoki@gmail.com>
2025-04-08 20:56:40 +0100

    Enhance ManagementSection layout and styling for improved responsiveness and visual presentation

--- src/app/components/home/ManagementSection.tsx
+++ src/app/components/home/ManagementSection.tsx
@@ -19,19 +19,19 @@ const ManagementCard: React.FC<CardProps> = ({ name, image, titles }) => {
 
     return (
         <>
-            <div className={"card bg-transparent w-51 shrink-0 rounded-none shadow-sm " + openSans.className}>
+            <div className={"card bg-transparent w-51 md:w-60 lg:w-70 xl:w-90 shrink-0 rounded-none shadow-sm " + openSans.className}>
                 <figure>
                     <Image
-                        className="w-full h-50"
+                        className="w-full h-50 md:h-60 lg:h-70 xl:h-86.5"
                         src={image}
                         alt="Director" />
                 </figure>
-                <div className="card-body px-3 py-2 text-base-content">
-                    <h2 className="card-title text-sm font-semibold">{name}</h2>
+                <div className="card-body px-3 xl:px-5 py-2 xl:py-4 text-base-content">
+                    <h2 className="card-title text-sm md:text-lg lg:text-xl xl:text-2xl font-semibold">{name}</h2>
 
                     <div>
                         {titles.map((title, index) => (
-                            <p key={name + "title" + index} className="text-[10px]">
+                            <p key={name + "title" + index} className="text-[10px] md:text-base lg:text-lg xl:text-xl md:font-semibold">
                                 {title}
                             </p>
                         ))}
@@ -46,13 +46,13 @@ const ManagementSection: React.FC = () => {
 
     return (
         <>
-            <section className="bg-neutral flex flex-col gap-4 my-15">
-                <div className="flex flex-col px-10">
-                    <h4 className="text-sm text-primary font-medium">MANAGEMENT TEAM</h4>
-                    <p className={"text-sm " + openSans.className}>Ubotex Limited is powered by three directors, Legal Adviser, External Auditor and Departmental Heads.</p>
+            <section className="bg-neutral flex flex-col gap-4 md:gap-11 my-15 md:my-25 lg:my-30 xl:my-40">
+                <div className="flex flex-col px-10 md:px-36">
+                    <h4 className="text-sm md:text-3xl lg:text-6xl/20 text-primary font-medium">MANAGEMENT TEAM</h4>
+                    <p className={"text-sm md:text-base " + openSans.className}>Ubotex Limited is powered by three directors, Legal Adviser, External Auditor and Departmental Heads.</p>
                 </div>
 
-                <div className={"flex flex-row gap-5 py-0.25 pl-10 overflow-x-auto " + openSans.className}>
+                <div className={"flex flex-row gap-5 md:justify-center md:gap-8 py-0.25 pl-10 md:p-0 overflow-x-auto md:overflow-visible " + openSans.className}>
                     <ManagementCard
                         image={elderUbongImage}
                         name="Elder Ubong Obot"
