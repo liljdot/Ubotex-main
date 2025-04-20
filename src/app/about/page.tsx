e37396e1c946bfc3b2f02edd7c5e40d38e7b4d44
liljdot <jehuoki@gmail.com>
2025-04-20 19:38:23 +0100

    Refactor About page layout for improved responsiveness and styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,8 +13,8 @@ const About: React.FC = () => {
 
     const SubSection: React.FC<SubSectionProps> = ({ title, children }) => {
         return (
-            <div className="flex flex-col gap-4 xl:gap-7.5 text-base-content ">
-                <h2 className="text-2xl xl:text-3xl">{title}</h2>
+            <div className="flex flex-col gap-4 md:gap-5 lg:gap-6 xl:gap-7.5 text-base-content ">
+                <h2 className="text-2xl md:text-3xl">{title}</h2>
                 <div className={"text-sm/7 md:text-base/10 md:text-justify " + openSans.className}>{children}</div>
             </div>
         )
@@ -27,12 +27,12 @@ const About: React.FC = () => {
                     <section
                         className="
                             flex flex-col 
-                            gap-6 xl:gap-23
+                            gap-6 md:gap-10 lg:gap-16 xl:gap-23
                             items-center
                             pt-19 md:pt-28 lg:pt-37.5
-                            pb-9 xl:pb-27">
+                            pb-9 lg:pb-18 xl:pb-27">
 
-                        <h1 className="text-primary text-6xl/20 xl:text-[109px] text-center">
+                        <h1 className="text-primary text-6xl/20 md:text-7xl lg:text-8xl xl:text-[109px] text-center">
                             ABOUT US
                         </h1>
 
@@ -40,8 +40,8 @@ const About: React.FC = () => {
                             className="
                                 flex flex-col
                                 text-center
-                                gap-15 xl:gap-40
-                                px-7.5 xl:px-34">
+                                gap-15 md:gap-24 lg:gap-32 xl:gap-40
+                                px-7.5 md:px-14 lg:px-24 xl:px-34">
                             <SubSection title="Our Legacy of Excellence">
                                 Founded on September 17, 2002, UBOTEX LIMITED is a proudly Nigerian company built on a foundation of quality, reliability, and innovation. What started as a cement distribution and building materials supply business has grown into a diversified enterprise, spanning construction, transportation, hospitality, and luxury retail. Our commitment to excellence has made us a trusted name in Nigeria’s construction industry, ensuring the supply of top-grade cement, ceiling boards, rods, nails, Cameroon zinc, and more. With a strong logistics network, we efficiently transport and distribute building materials across the country.
                             </SubSection>
@@ -60,7 +60,7 @@ const About: React.FC = () => {
                             </SubSection>
 
                             <SubSection title="Our Mission">
-                                At UBOTEX LIMITED, we are committed to sustainable business practices, consistently delivering exceptional service through innovation, motivated professionals, and cutting-edge facilities. Our goal is to remain an industry leader, setting new standards for excellence in every sector we operate. 
+                                At UBOTEX LIMITED, we are committed to sustainable business practices, consistently delivering exceptional service through innovation, motivated professionals, and cutting-edge facilities. Our goal is to remain an industry leader, setting new standards for excellence in every sector we operate.
                                 <br />
                                 Welcome to UBOTEX LIMITED—where strength, luxury, and quality meet.
                             </SubSection>
